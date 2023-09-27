feba478dc1ad9c8b74253f1e8f840cc50ee4fda8
Mark Nair <marknair@me.com>
2023-09-27 09:11:04 -0500

    Added cards total

--- script.js
+++ script.js
@@ -8,8 +8,17 @@ let responseMessage = ""
 let gameMessage = document.getElementById("message-element")
 console.log(gameMessage)
 
+// I'll store the total-element <p> in a new variable called theTotal.
+let theTotal = document.getElementById("total-element")
+
+// Store the cards in a <p> variable.
+let theCards = document.getElementById("cards-element")
 
 function startGame() {
+    //Show the cards on the page
+    theCards.textContent = "Cards: " + firstCard + " " + secondCard
+    //Display theTotal on the page
+    theTotal.textContent = "Sum: " + sum
     if (sum < 21) {
         responseMessage = "Do you want another card?"
     } else if (sum === 21) {
@@ -20,6 +29,10 @@ function startGame() {
         isAlive = false
     }
     gameMessage.textContent = responseMessage
+}
+
+function newCard() {
+    console.log("Drawing a new card from the deck.")
 }
 
 
