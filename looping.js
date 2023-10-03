c97e646b36d8376512657c65225168b74bb6e9c6
Mark Nair <marknair@me.com>
2023-10-03 09:10:37 -0500

    Added looping.js

---
+++ looping.js
@@ -0,0 +1,11 @@
+let messages = [
+    "Hey",
+    "How are you?",
+    "I need donut",
+    "Make me a sandwich",
+    "Where's the candy, creeper?"
+]
+
+for (let i = 0; i < messages.length; i++) {
+    console.log(messages[i])
+}
